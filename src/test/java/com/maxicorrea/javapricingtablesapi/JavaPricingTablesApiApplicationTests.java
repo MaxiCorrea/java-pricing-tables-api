a3bb2307228314cac694785022b0062eb09c09aa
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-02-23 22:08:56 -0300

    Initial Commit

---
+++ src/test/java/com/maxicorrea/javapricingtablesapi/JavaPricingTablesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxicorrea.javapricingtablesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPricingTablesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
